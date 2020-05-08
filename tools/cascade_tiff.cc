ff4b4f75a5586506eeacc7cffa3cb0e868cdc1db
JoshuaLandgraf <jland@cs.utexas.edu>
2020-05-08 04:51:52 +0000

    well, can run mips using lib. now going to try mips w yield.

--- tools/cascade_tiff.cc
+++ tools/cascade_tiff.cc
@@ -43,161 +43,13 @@ using namespace std;
 
 namespace cascade::cascade_tiff {
 
-//__attribute__((unused)) auto& g1 = Group::create("Cascade Runtime Options");
-//auto& march = StrArg<string>::create("--march")
-//  .usage("sw|de10|f1|ulx3s")
-//  .description("Target architecture")
-//  .initial("sw");
-//auto& fopen_dirs = StrArg<string>::create("-F")
-//  .usage("<path1>:<path2>:...:<pathn>")
-//  .description("Paths to search when resolving $fopen() statements")
-//  .initial("");
-//auto& inc_dirs = StrArg<string>::create("-I")
-//  .usage("<path1>:<path2>:...:<pathn>")
-//  .description("Paths to search when resolving `include directives")
-//  .initial("");
-//auto& input_path = StrArg<string>::create("-e")
-//  .usage("path/to/file.v")
-//  .description("Read input from file");
-//
-//__attribute__((unused)) auto& g2 = Group::create("Compiler Server Options");
-//auto& compiler_host = StrArg<string>::create("--compiler_host")
-//  .usage("<host>")
-//  .description("Location of quartus / vivado server")
-//  .initial("localhost");
-//auto& compiler_port = StrArg<uint32_t>::create("--compiler_port")
-//  .usage("<port>")
-//  .description("Location of quartus / vivado server")
-//  .initial(9900);
-//auto& compiler_fpga = StrArg<uint32_t>::create("--compiler_fpga")
-//  .usage("<fpga>")
-//  .description("Index of target FPGA for deployment")
-//  .initial(0);
-//
-//__attribute__((unused)) auto& g3 = Group::create("Logging Options");
-//auto& profile = StrArg<int>::create("--profile")
-//  .usage("<n>")
-//  .description("Number of seconds to wait between profiling events; setting n to zero disables profiling; only effective with --enable_info")
-//  .initial(0);
-//auto& enable_info = FlagArg::create("--enable_info")
-//  .description("Turn on info messages");
-//auto& disable_warning = FlagArg::create("--disable_warning")
-//  .description("Turn off warning messages");
-//auto& disable_error = FlagArg::create("--disable_error")
-//  .description("Turn off error messages");
-//auto& enable_log = FlagArg::create("--enable_log")
-//  .description("Prints debugging information to log file");
-//
-//__attribute__((unused)) auto& g4 = Group::create("Optimization Options");
-//auto& disable_inlining = FlagArg::create("--disable_inlining")
-//  .description("Prevents cascade from inlining modules");
-//auto& open_loop_target = StrArg<size_t>::create("--open_loop_target")
-//  .usage("<n>")
-//  .description("Maximum number of seconds to run in open loop for before transferring control back to runtime")
-//  .initial(1);
-//
-//__attribute__((unused)) auto& g5 = Group::create("REPL Options");
-//auto& disable_repl = FlagArg::create("--disable_repl")
-//  .description("Disables the REPL and treats user input as stdin");
-//
-//class inbuf : public streambuf {
-//  public:
-//    inbuf(streambuf* sb) : streambuf() { 
-//      sb_ = sb;
-//      prompt_ = false;
-//    }
-//    ~inbuf() override = default;
-//
-//  private:
-//    streambuf* sb_;
-//    bool prompt_;
-//
-//    int sync() override {
-//      return sb_->pubsync();
-//    }
-//    int_type uflow() override {
-//      const auto res =  sb_->sbumpc();
-//      prompt(res);
-//      prompt_ = false;
-//      return res;
-//    }
-//    int_type underflow() override {
-//      const auto res = sb_->sgetc();
-//      prompt(res);
-//      return res;
-//    }
-//    void prompt(int_type c) {
-//      if ((c == '\n') && !prompt_) {
-//        prompt_ = true;
-//        cout << ">>> ";
-//        cout.flush();
-//      }
-//    }
-//};
-//
-//class outbuf : public streambuf {
-//  public:
-//    outbuf(const string& color = "") : streambuf() {
-//      color_ = color;
-//    }
-//    ~outbuf() override = default;
-//
-//  private:
-//    string color_;
-//    stringstream ss_;
-//
-//    int_type overflow(int_type c = traits_type::eof()) override {
-//      ss_.put(c);
-//      if (c == '\n') {
-//        ss_ << ((color_ != "") ? "\033[00m" : "") << ">>> " << color_;
-//      }
-//      return c;
-//    }
-//    int_type sync() override {
-//      cout << color_ << ss_.str() << ((color_ != "") ? "\033[00m" : "");
-//      cout.flush();
-//      ss_.str(string());
-//      return 0;
-//    }
-//};
-
 // Allocate cascade on the heap so we can guarantee that it's torn down before
 // stack or static variables.
   Cascade* cascade_ = nullptr;
 
-//// Signal Handlers:
-//void int_handler(int sig) {
-//  (void) sig;
-//  ::cascade_->request_stop();
-//}
-//void segv_handler(int sig) {
-//  (void) sig;
-//  cerr << "\033[31mCASCADE SHUTDOWN UNEXPECTEDLY\033[00m" << endl;
-//  cerr << "\033[31mPlease rerun with --enable_log and forward log file to developers\033[00m" << endl;
-//  exit(1);
-//}
-
 } // namespace
 
 int main(int argc, char** argv) {
-  //// Parse command line
-  //Simple::read(argc, argv);
-  //
-  //// Wrap cin in inbuf (re-prints the prompt when the user types \n)
-  //inbuf ib(cin.rdbuf());
-  //cin.rdbuf(&ib);
-  //
-  //// Install signal handlers
-  //{ struct sigaction action;
-  //  memset(&action, 0, sizeof(action));
-  //  action.sa_handler = ::segv_handler;
-  //  sigaction(SIGSEGV, &action, nullptr);
-  //}
-  //{ struct sigaction action;
-  //  memset(&action, 0, sizeof(action));
-  //  action.sa_handler = ::int_handler;
-  //  sigaction(SIGINT, &action, nullptr);
-  //}
 
   // Create a new cascade
   cascade::cascade_tiff::cascade_ = new Cascade();
@@ -217,50 +69,19 @@ int main(int argc, char** argv) {
   
 
   //// Map standard streams to colored outbufs
-  //if (::disable_repl.value()) {
   cascade::cascade_tiff::cascade_->set_stdin(cin.rdbuf());
-  //}
-  //cascade::cascade_tiff::cascade_->set_stdout(new outbuf());
-  //if (!::disable_error.value()) {
-  //  cascade::cascade_tiff::cascade_->set_stderr(new outbuf("\033[31m"));
-  //}
-  //if (!::disable_warning.value()) {
-  //  cascade::cascade_tiff::cascade_->set_stdwarn(new outbuf("\033[33m"));
-  //}
-  //if (::enable_info.value()) {
-  //  cascade::cascade_tiff::cascade_->set_stdinfo(new outbuf("\033[37m"));
-  //}
   auto* fb = new filebuf();
   fb->open("tiff_cascade.log", ios::app | ios::out);
   cascade::cascade_tiff::cascade_->set_stdlog(fb);
 
   //// Print the initial prompt
   //cout << ">>> ";
-  
-  cout << "made it to here" << endl;
 
   // Start cascade, and read the march file and -e file (if provided)
   cascade::cascade_tiff::cascade_->run();
   *cascade::cascade_tiff::cascade_ << "`include \"share/cascade/march/regression/f1_minimal.v\"\n"
 	      << "`include \"share/cascade/test/benchmark/mips32/run_bubble_128_1024.v\"" << endl;
-  //cascade::cascade_tiff::cascade_->stop_now();
-  //
-  //// Switch to reading from cin if the REPL wasn't disable and wait for finish
-  //if (!::disable_repl.value()) {
-  //  cascade::cascade_tiff::cascade_->rdbuf(cin.rdbuf());
-  //}
-  //cascade::cascade_tiff::cascade_->run();
-  //cascade::cascade_tiff::cascade_->wait_for_stop();
-  //
-  //// If cascade isn't finished by now, it's because we've caught a signal.
-  //// Either way, we can delete it now.
-  //if (!cascade::cascade_tiff::cascade_->is_finished()) {
-  //  cerr << "\033[31mCaught Signal\033[00m" << endl;
-  //}
-  //delete cascade::cascade_tiff::cascade_;
-  //
-  //cout << "Goodbye!" << endl;
-    cout << "made it to here 2" << endl;
+
   cascade::cascade_tiff::cascade_->flush();
 
   // Sleep for 30 seconds
