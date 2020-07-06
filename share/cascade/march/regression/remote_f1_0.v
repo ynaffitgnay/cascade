1b983ae35f2fc2391708a30666fe3734b344e72e
Tiffany Yang <tiffanyyang@utexas.edu>
2020-07-06 22:52:42 +0000

    Define remote f1 marches for jitting

---
+++ share/cascade/march/regression/remote_f1_0.v
@@ -0,0 +1,11 @@
+`ifndef __SHARE_CASCADE_MARCH_REMOTE_F1_0_V
+`define __SHARE_CASCADE_MARCH_REMOTE_F1_0_V
+
+`include "share/cascade/stdlib/stdlib.v"
+
+(*__target="sw;f1",__loc="local;127.0.0.1:8800"*)
+Root root();
+
+Clock clock();
+
+`endif
---
+++ share/cascade/march/regression/remote_f1_1.v
@@ -0,0 +1,11 @@
+`ifndef __SHARE_CASCADE_MARCH_REMOTE_F1_1_V
+`define __SHARE_CASCADE_MARCH_REMOTE_F1_1_V
+
+`include "share/cascade/stdlib/stdlib.v"
+
+(*__target="sw;f1",__loc="local;127.0.0.1:8801"*)
+Root root();
+
+Clock clock();
+
+`endif
