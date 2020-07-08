222744919fd86a096d3ec0b757542ca99a30d8da
Tiffany Yang <tiffanyyang@utexas.edu>
2020-07-08 19:22:32 +0000

    Added additional remote f1 marches for retargeting, increased number of MIPS iterations to INTMAX to have long enough runtime

---
+++ share/cascade/march/regression/remote_f1_3.v
@@ -0,0 +1,11 @@
+`ifndef __SHARE_CASCADE_MARCH_REMOTE_F1_3_V
+`define __SHARE_CASCADE_MARCH_REMOTE_F1_3_V
+
+`include "share/cascade/stdlib/stdlib.v"
+
+(*__target="sw;f1",__loc="local;127.0.0.1:8803"*)
+Root root();
+
+Clock clock();
+
+`endif
